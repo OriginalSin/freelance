661d20d9bd768869a1f4d7e684d88e46ecec5d0f
Serg <saleks@scanex.ru>
2017-04-23 11:00:15 +0300

    styles

--- app.js
+++ app.js
@@ -1,88 +1,87 @@
 var map = L.map('map').setView([20, -17.59], 2);
+L.Icon.Default.imagePath = './';
+var iconUrl = L.Icon.Default.imagePath + 'sia.jpg';
+L.Marker = L.Marker.extend({
+	options: {
+		icon: new L.Icon.Default({
+			iconUrl: iconUrl,
+			iconSize: [50, 50],
+			iconAnchor: [7, 37],
+			popupAnchor: [3, -25],
+			shadowUrl: iconUrl,
+			shadowSize: [0, 0],
+			shadowAnchor: [0, 0]
+		})
+	}
+});
 
 var osm = L.tileLayer('https://tilessputnik.ru/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 }).addTo(map);
+var viewItems = document.getElementsByClassName('viewItems')[0],
+	viewItemsCount = document.getElementsByClassName('viewItemsCount')[0];
 
-var prev = document.getElementsByClassName('prev');
+fetch('sia.json').then(function(response) {
+		return response.json();
+	}).then(function(json) {
+		var featureCollection = utils.getFeatureCollection(json);
+		var markers = L.markerClusterGroup({
+			showCoverageOnHover: false,
+			disableClusteringAtZoom: 16
+		});
 
-var worker = new Worker('worker.js'),
-    lastID = 0,
-	sendMessage = function(opt) {
-		opt.id = ++lastID;
-console.log('sendMessage:', opt);
-		worker.postMessage(opt);
-		return lastID;
-	};
+		var geoJsonLayer = L.geoJson(featureCollection, {
+			onEachFeature: function (feature, layer) {
+				var props = feature.properties;
+				var html = '<strong>' + props.Name + '</strong>\
+					<br>' + props.Address + '\
+					<br>' + props['Phone Number'] + '\
+					<a href="' + props.Website + '">WEBSITE</a>';
+				layer.bindPopup(html);
+			}
+		});
+		markers.addLayer(geoJsonLayer);
 
-worker.onmessage = function(e) {
-	var mess = e.data;
-	console.log('onmessage:', mess, e);
-};
-prev[0].onclick = function(ev) {
-	sendMessage({
-		cmd: 'getMapProperties',
-		apiKey: 'Z2SSNR87N4',
-		mapID: '24A629C7563742B49BBCC01D7D27B8DB'
+		map.addLayer(markers);
+		map.fitBounds(markers.getBounds());
+		map.on('moveend', function(ev) {
+			var count = 0,
+				bbox = map.getBounds();
+			geoJsonLayer.getLayers().forEach(function(it) {
+				var flag = bbox.contains(it.getLatLng()),
+					node = it.feature.dom;
+				if (flag) {
+					count++;
+					L.DomUtil.removeClass(node, 'gmx-hidden');
+				} else {
+					L.DomUtil.addClass(node, 'gmx-hidden');
+				}
+			});
+			viewItemsCount.innerHTML = count;
+		});
 	});
+var utils = {
+	getFeatureCollection: function(json) {
+		var columns = json.columns;
+		return geoJsonData = {
+			"type": "FeatureCollection", 
+			"features": json.rows.map(function(arr, i) {
+				var nm = i + 1,
+					props = arr.reduce(function(p, c, ind) {
+						p[columns[ind]] = c;
+						return p;
+					}, {});
+				var node = L.DomUtil.create('div', 'point-detail vcard');
+				node.innerHTML = '<div class="icon"><div class="name fn org overflow-controlled" title="' + props.Name + '"><a href="' + props.Website + '" target="none">' + props.Name + '</a></div><div class="caption">' + props.Description + '</div></div><div class="metadata"><strong><div class="address adr overflow-controlled" title="' + props.Address + '">' + props.Address + '</div><div class="phone tel overflow-controlled">(202) 543-9300</div><div class="url overflow-controlled"><a href="' + props.Website + '" target="_blank">Website</a></div></strong></div>';
+				viewItems.appendChild(node);
+				return {
+						"type": "Feature", "id": nm,
+						"dom": node,
+						"properties": props,
+						"geometry": { "type": "Point", "coordinates": [arr[1], arr[0] ] }
+				}
+			})
+		};
+	}
 };
-
-
-var cmdID = sendMessage({
-	apiKey: 'Z2SSNR87N4',
-	cmd: 'getSessionKey'
-});
-// sendMessage({
-	// cmd: 'getMapProperties',
-	// apiKey: 'Z2SSNR87N4',
-	// mapID: '946GH'
-// });
-
-// sendMessage({
-	// cmd: 'getMapProperties',
-	// apiKey: 'Z2SSNR87N4',
-	// mapID: '24A629C7563742B49BBCC01D7D27B8DB'
-// });
-
-//Load all the layers from GeoMixer map and add them to Leaflet map
-//L.gmx.loadMap('24A629C7563742B49BBCC01D7D27B8DB', {leafletMap: map});
-/*
-var myGmxTree = L.control.gmxTree({
-	position: 'mapTree',
-	mapID: 'WX3Q3'
-	// mapID: '24A629C7563742B49BBCC01D7D27B8DB'
-});
-myGmxTree
-	.addTo(map)
-	.on('selected', myGmxTree.nodeSelect)			// async: показать слой на карте (создать слой если еще не создан)
-	.on('deselected', myGmxTree.nodeDeselect)		// скрыть слой с карты
-	.on('expanded', function (ev) {					// async: открыть группу
-		console.log('__expanded___', ev);
-	})
-	.on('contextmenu', function (ev) {				// контекстное меню
-		console.log('_____', ev);
-		var node = ev.treeNode,
-			// originalEvent = node.originalEvent,
-			// gmxOptions = node.gmxOptions,
-			type = node.type,
-			arr = [];
-		if (type === 'layer') {
-			// if (dataSource) // меню вьюшки
-			arr = [	// меню источника данных слоя 
-				{text: 'Свойства', callback: function (ev) { console.log('Свойства', ev); }},
-				{separator: true},
-				{text: 'Копировать стиль', callback: function (ev) { console.log('Копировать стиль', ev); }},
-				{text: 'Добавить объект', callback: function (ev) { console.log('Добавить объект', ev); }}
-			];
-		} else if (type === 'group') {
-			arr = [	// меню группы
-				{text: 'Свойства', callback: function (ev) { console.log('Свойства', ev); }},
-				{text: 'Добавить группу', callback: function (ev) { console.log('Добавить группу', ev); }},
-				{text: 'Удалить', callback: function (ev) { console.log('Удалить', ev); }}
-			];
-		}
-		myGmxTree.setContextMenuItems(arr);
-	});
-
-*/
