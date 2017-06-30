5a177401ce00e1fec2a7b9ead524111009f34fb1
Serg <saleks@scanex.ru>
2017-06-30 13:44:11 +0300

    sia search

--- sia/js/app.js
+++ sia/js/app.js
@@ -21,8 +21,8 @@ var osm = L.tileLayer('https://tilessputnik.ru/{z}/{x}/{y}.png', {
 }).addTo(map);
 var viewItems = document.getElementsByClassName('viewItems')[0],
 	viewItemsCount = document.getElementsByClassName('viewItemsCount')[0];
-
-fetch('sia.json').then(function(response) {
+  // export from csv http://www.csvjson.com/csv2json
+fetch('data/sia6.json').then(function(response) {
 		return response.json();
 	}).then(function(json) {
 		var featureCollection = utils.getFeatureCollection(json);
@@ -31,7 +31,7 @@ fetch('sia.json').then(function(response) {
 			disableClusteringAtZoom: 16
 		});
 
-		var geoJsonLayer = L.geoJson(featureCollection, {
+		var geoJsonLayer = utils.geoJsonLayer = L.geoJson(featureCollection, {
 			onEachFeature: function (feature, layer) {
 				var props = feature.properties,
 					img = props.Image ? '<img src="' + props.Image + '" />' : '',
@@ -47,12 +47,73 @@ fetch('sia.json').then(function(response) {
 
 		map.addLayer(markers);
 		map.fitBounds(markers.getBounds());
-		map.on('moveend', function(ev) {
-			var count = 0,
-				bbox = map.getBounds();
-			geoJsonLayer.getLayers().forEach(function(it) {
-				var flag = bbox.contains(it.getLatLng()),
-					node = it.feature.dom;
+		map.on('moveend', utils.setFilter);
+		// map.on('moveend', function(ev) {
+			// var count = 0,
+				// bbox = map.getBounds();
+			// geoJsonLayer.getLayers().forEach(function(it) {
+				// var flag = bbox.contains(it.getLatLng()),
+					// node = it.feature.dom;
+				// if (flag) {
+					// count++;
+					// L.DomUtil.removeClass(node, 'gmx-hidden');
+				// } else {
+					// L.DomUtil.addClass(node, 'gmx-hidden');
+				// }
+			// });
+			// viewItemsCount.innerHTML = count;
+		// });
+	});
+
+var utils = {
+	buttons: ['zipcode', 'sButton', 'searchBARS', 'searchALL', 'searchSTORE'],
+	getButtonType: function(button) {
+		for (var i = 0, len = utils.buttons.length; i < len; i++) {
+			var type = utils.buttons[i];
+			if ( L.DomUtil.hasClass(button, type)) {
+				return type;
+			}
+		}
+		return null;
+	},
+	setCurrentType: function(type) {
+		for (var i = 2, len = utils.buttons.length; i < len; i++) {
+			var name = utils.buttons[i];
+				fname = type === name ? 'addClass' : 'removeClass';
+			L.DomUtil[fname](document.getElementsByClassName(name)[0], 'current');
+		}
+	},
+	setFilter: function(ev) {
+		var button = ev ? ev.target : null,
+			type = button && button.className ? utils.getButtonType(button) : 'searchALL',
+			count = 0,
+			flayers = utils.geoJsonLayer.getLayers(),
+			bbox = map.getBounds();
+
+		if (type === 'sButton') {
+			var val = document.getElementsByClassName('zipcode')[0].value,
+				fname = val.match(/[^\d]/) ? 'Address' : 'Zipcode';
+			for (var i = 0, len = flayers.length; i < len; i++) {
+				var it = flayers[i],
+					pv = String(it.feature.properties[fname]);
+				if (pv.indexOf(val) !== -1) {
+					map.setView(it.getLatLng(), 17);
+					return;
+				}
+			}
+		} else if (type.indexOf('search') !== -1) {
+			utils.setCurrentType(type);
+			flayers.forEach(function(it) {
+				var feature = it.feature,
+					props = feature.properties,
+					node = feature.dom,
+					flag = bbox.contains(it.getLatLng());
+
+				if (type === 'searchBARS') {
+					if (props.Category !== 'DRINK') { flag = false; }
+				} else if (type === 'searchSTORE') {
+					if (props.Category !== 'STORE') { flag = false; }
+				}
 				if (flag) {
 					count++;
 					L.DomUtil.removeClass(node, 'gmx-hidden');
@@ -61,29 +122,28 @@ fetch('sia.json').then(function(response) {
 				}
 			});
 			viewItemsCount.innerHTML = count;
-		});
-	});
-var utils = {
+		}
+		console.log(type)
+	},
 	getFeatureCollection: function(json) {
-		var columns = json.columns;
-		return geoJsonData = {
+		// var columns = json.columns;
+		return {
 			"type": "FeatureCollection", 
-			"features": json.rows.map(function(arr, i) {
-				var nm = i + 1,
-					props = arr.reduce(function(p, c, ind) {
-						p[columns[ind]] = c;
-						return p;
-					}, {});
+			"features": json.map(function(it, i) {
 				var node = L.DomUtil.create('div', 'point-detail vcard');
-				node.innerHTML = '<div class="icon"><div class="name fn org overflow-controlled" title="' + props.Name + '"><a href="' + props.Website + '" target="none">' + props.Name + '</a></div><div class="caption">' + props.Description + '</div></div><div class="metadata"><strong><div class="address adr overflow-controlled" title="' + props.Address + '">' + props.Address + '</div><div class="phone tel overflow-controlled">(202) 543-9300</div><div class="url overflow-controlled"><a href="' + props.Website + '" target="_blank">Website</a></div></strong></div>';
+				node.innerHTML = '<div class="icon"><div class="name fn org overflow-controlled" title="' + it.Name + '"><a href="' + it.Website + '" target="none">' + it.Name + '</a></div><div class="caption">' + it.Description + '</div></div><div class="metadata"><strong><div class="address adr overflow-controlled" title="' + it.Address + '">' + it.Address + '</div><div class="phone tel overflow-controlled">(202) 543-9300</div><div class="url overflow-controlled"><a href="' + it.Website + '" target="_blank">Website</a></div></strong></div>';
 				viewItems.appendChild(node);
 				return {
-						"type": "Feature", "id": nm,
+						"type": "Feature", "id": i + 1,
 						"dom": node,
-						"properties": props,
-						"geometry": { "type": "Point", "coordinates": [arr[1], arr[0] ] }
+						"properties": it,
+						"geometry": { "type": "Point", "coordinates": [it.Lng, it.Lat ] }
 				}
 			})
 		};
 	}
 };
+
+utils.buttons.forEach(function(type) {
+	L.DomEvent.on(document.getElementsByClassName(type)[0], 'click', utils.setFilter);
+});
